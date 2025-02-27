aea4deb7534e41ddbf0a445597a5d984ccdcae81
giihdot <giihferreira0708@gmail.com>
2025-02-27 14:21:17 -0300

    pages

---
+++ src/pages/Duarte.jsx
@@ -0,0 +1,29 @@
+import './Profiles.css'
+
+function Duarte() {
+
+    return (
+        <div>
+           <h3> MARIA CLARA DUARTE </h3>
+           <img src="Duarte_Img.jpeg"> </img>
+           <p> Coração Forte, Alma Sensível
+            Maria, aos 16 anos, é um misto de força e sensibilidade. De personalidade marcante, ela sente o mundo de maneira 
+            intensa—cada emoção, cada detalhe, cada gesto. Sua melancolia não é fraqueza, mas sim uma profundidade rara, 
+            que a faz enxergar além do que é visível. Ela carrega no olhar um oceano de sentimentos e no coração um amor 
+            imenso por aqueles que fazem parte de sua vida.
+
+            Cuidadosa e carinhosa, Maria tem um instinto protetor natural. Sempre atenta às necessidades dos outros, está 
+            disposta a ajudar e acolher, oferecendo palavras doces e um abraço sincero quando alguém precisa. Mas se engana 
+            quem pensa que sua doçura significa fragilidade—quando mexem com quem ela ama, seu temperamento muda, e sua 
+            paciência se esgota rapidamente. Ela pode ser calma, mas não leva desaforo para casa quando o assunto é proteger 
+            aqueles que lhe são importantes.
+
+            Sua fé cristã é sua base, o que lhe dá força nos momentos difíceis e acalma seu coração quando o mundo parece pesado.
+            Para Maria, Deus é seu refúgio, e é Nele que ela encontra paz, propósito e esperança.
+
+            Com um coração que sente demais e uma alma que ama com intensidade, Maria é única—forte, sensível e 
+            inegavelmente verdadeira. </p>
+        </div>
+    )
+}
+export default Duarte
---
+++ src/pages/Kamilly.jsx
@@ -0,0 +1,27 @@
+import './Profiles.css'
+
+function Kamilly() {
+
+    return (
+        <div>
+           <h3> KAMILLY BARRA </h3>
+           <img src="Kamilly_Img.jpeg"> </img>
+           <p> Entre a Razão e a Emoção
+            Kamilly, aos 17 anos, carrega em si uma profundidade rara. De personalidade forte e olhar crítico, 
+            ela enxerga o mundo com uma sensibilidade que poucos compreendem. Sempre refletindo sobre a vida e suas 
+            complexidades, sua mente está em constante análise—questionando, observando e buscando significados além do óbvio.
+
+            Melancólica por natureza, Kamilly sente tudo intensamente. As emoções não são apenas passageiras para ela; 
+            são ondas profundas que moldam sua visão de mundo. Mas, ao mesmo tempo que carrega essa intensidade, 
+            há também um lado cuidadoso e acolhedor em seu coração. Com quem ama, é protetora, sempre pronta para oferecer apoio,
+            conselhos sinceros e um ombro seguro.
+
+            Sua fé cristã é seu alicerce, a luz que a guia mesmo nos momentos mais sombrios. Para Kamilly, Deus está presente 
+            não apenas nos momentos de alegria, mas também nos silêncios e nas incertezas. Sua espiritualidade a fortalece e 
+            lhe dá propósito, ajudando-a a equilibrar sua mente crítica com a esperança de dias melhores.
+
+            Autêntica e intensa, Kamilly não se encaixa em rótulos—ela os desafia. </p>
+        </div>
+    )
+}
+export default Kamilly
